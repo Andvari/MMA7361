4e8ecb891555d15e8e44d64641cdb012055e9acb
Nemo <dedmazai74@mail.ru>
2012-09-24 15:02:47 +0400

    First Commit

---
+++ mma7361.cpp
@@ -0,0 +1,10 @@
+/*
+ * mma7361.cpp
+ *
+ *  Created on: Sep 24, 2012
+ *      Author: nemo
+ */
+
+
+
+
---
+++ mma7361.h
@@ -0,0 +1,14 @@
+/*
+ * mma7361.h
+ *
+ *  Created on: Sep 24, 2012
+ *      Author: nemo
+ */
+
+#ifndef MMA7361_H_
+#define MMA7361_H_
+
+
+
+
+#endif /* MMA7361_H_ */
